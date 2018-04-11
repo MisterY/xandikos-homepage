fc216aa536b3227ae8e50dc970cfd35bd6873e4a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-04-11 02:01:08 +0100

    More settings.

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,24 +12,7 @@ TIMEZONE = 'Etc/UTC'
 
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
 THEME = 'pjport'
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
@@ -41,3 +24,53 @@ JINJA_ENVIRONMENT = {
     'extensions': ['jinja2.ext.i18n'],
 }
 PLUGIN_PATHS = ['plugins', 'sitemap', 'assets']
+
+# Blogroll
+LINKS = (('Code', 'https://jelmer.uk/code/xandikos'),
+         ('GitHub', 'https://www.github.com/jelmer/xandikos'))
+
+# Social widget
+SOCIAL = (
+)
+
+DEFAULT_PAGINATION = 10
+
+# Menu
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+MENUITEMS = (
+    ('', ''), # this item is to show link on top and bottom of page
+)
+
+#
+# SITEMAP PLUGIN
+#
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+DEFAULT_DATE_FORMAT = ('- %d -<br/>%B<br/>%Y')
+
+STATIC_PATHS = []
+EXTRA_PATH_METADATA = {}
+
+# about me
+ABOUT_ME = None # showed on left side of footer
+
+# contacts
+CONTACT_EMAIL = 'jelmer@jelmer.uk'
+CONTACT_PHONE = None
+CONTACT_CITY = None
+
+# GOOGLE ANALYTICS (set to  None if don't want it)
+GOOGLE_ANALYTICS = None
